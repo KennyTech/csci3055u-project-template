8cd724524dadac84b0a25a14da4924de702bcb63
Kenny Le <35461997+KennyTech@users.noreply.github.com>
2018-12-11 23:33:35 -0500

    Create HelloWorld.scala
    added basic syntax file

---
+++ basic-syntax/HelloWorld.scala
@@ -0,0 +1,9 @@
+object HelloWorld extends App {
+  println("Hello World")
+}
+
+// The purpose of this file shows how Scala does not need a class or main function
+// It can run using an object (singleton), producing "Hello World" as output.
+// Can be run using 
+// > scalac HelloWorld.scala
+// > scala HelloWorld
